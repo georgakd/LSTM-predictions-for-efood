677acd54d8f4f4d7b57c9823e1ce6ea45b129dec
Dimitra Georgakaki <dgeorgakaki@ubitech.eu>
2022-04-28 22:16:46 +0300

    Completed viewer api

--- exploration/utils.py
+++ exploration/utils.py
@@ -7,9 +7,7 @@ logger = logging.getLogger(__name__)
 
 def load_data(dir_name, filename):
     with open(os.path.join(os.getcwd(), dir_name, filename)) as f:
-
         data = pd.read_csv(f)
-    
     return data
 
 def data_preprocessor(dir_name, filename):
@@ -27,17 +25,21 @@ def data_preprocessor(dir_name, filename):
     df1 = df.dropna(subset=['visitor_type'])
     df_ret = df1.loc[df1["visitor_type"].str.contains('returning', case=False)]
 
-    # Calculate feature importance so as to eliminate the total dimensions of the problem    
+    # We will remove thesse columns as they do not contribute in the features of the prediction problem
+    df_minimal = df_ret.drop(columns=['platform', 'vendor_id', 'business_type', 'zipcode', 'cash', 'has_coupon', 'channel'])
+    
+    # Convert the x column to date
+    df_minimal['created_at'] = pd.to_datetime(df_minimal['created_at'])
+    df_minimal['created_at'] = df_minimal['created_at'].apply(lambda x: x.date())
 
-    # Find missing values if any. Check if the corresponding columns that contain null values can be used as features for the forecasting model.
-    # If the percentage of missing data is less than 20% of the whole dataset, we can use drop rows.
-    # TODO: If the percentage is larger, then we can use a more advanced imputation technique.
-    missing = df_ret.isnull().sum().sum()
-    if round(missing/len(df)) < 0.2:
-        # call drop null values
-        logger.info(f"There are {missing} missing rows for this dataset.")
- 
+    # Calculate orders and earnings per day
+    total_orders_per_day = df_minimal.groupby('created_at',as_index=False)['order_id'].count()
+    total_earnings_per_day = df_minimal.groupby('created_at',as_index=False)['total_order_value'].sum()
+    total_earnings_per_day = total_earnings_per_day.drop(columns='created_at')
+    
+    # Create the dataset for the LSTM model and add one more feature related to weekday-weekend
+    df_lstm = pd.concat([total_orders_per_day, total_earnings_per_day], axis=1)
+    df_lstm['weekday'] = df_lstm['created_at'].apply(lambda x: x.weekday()>=5)
 
 
-
-    return df_ret
+    return df_lstm
--- exploration/views.py
+++ exploration/views.py
@@ -1,15 +1,29 @@
-from rest_framework.response import Response
+from django.http import  HttpResponse
 from rest_framework.decorators import api_view
 from exploration.constants import FILENAME, DIR_NAME
 from exploration.utils import data_preprocessor
-
+import matplotlib.pyplot as plt
+import io
 
 
 @api_view(['GET'])
 def data_viewer(request):
-    df = data_preprocessor(DIR_NAME, FILENAME)
-
-    response_dict = {'hello', len(df)}
+    df_lstm = data_preprocessor(DIR_NAME, FILENAME)
     
+    fig = plt.figure()
+    df_lstm.plot(x="created_at", y=["order_id", "total_order_value"])
+    buf = io.BytesIO()
+    plt.xticks(rotation=30)
+    plt.title('Total orders and earnings per day')
+    plt.savefig(buf, format='png', dpi=100)
+    plt.close(fig)
 
-    return Response(response_dict)        
+
+    response = HttpResponse(buf.getvalue(), content_type='image/png')
+    return response
+
+@api_view(['GET'])
+def data_train(request):
+    df_lstm = data_preprocessor(DIR_NAME, FILENAME)
+
+    return response           
